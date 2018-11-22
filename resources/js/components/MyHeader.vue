0a068feb5499067a59e9f4853a910a7702888ea1
rabiul islam <rabiul@setup.technology>
2018-11-23 03:52:09 +0600

    basic component

---
+++ resources/js/components/MyHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul class="list-group offset-4 col-lg-4">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Morbi leo risus</li>
+        <li class="list-group-item">Porta ac consectetur ac</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
